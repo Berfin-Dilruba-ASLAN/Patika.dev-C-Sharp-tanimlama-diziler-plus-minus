c65ff096f56d3067a3f5b63c453b92f505ffa0e2
Berfin Dilruba ASLAN <98692629+Berfin-Dilruba-ASLAN@users.noreply.github.com>
2022-08-17 14:24:42 +0300

    Add files via upload

---
+++ tanımlama erişim döngülerle dizi kullanımı/plus_minus_tanimlama_dongulerlediziler/plus_minus_tanimlama_dongulerlediziler/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        var pozitifCount = 0;
+        var negatifCount = 0;
+        var sifirCount = 0;
+        for (var i = 0; i < arr.Count(); i++)
+        {
+            if (arr[i] > 0)
+            {
+                pozitifCount++;
+            }
+            else if (arr[i] < 0)
+            {
+                negatifCount++;
+            }
+            else
+            {
+                sifirCount++;
+            }
+        }
+        Console.WriteLine(((double)pozitifCount / arr.Count()).ToString("0.000000"));
+        Console.WriteLine(((double)negatifCount / arr.Count()).ToString("0.000000"));
+        Console.WriteLine(((double)sifirCount / arr.Count()).ToString("0.000000"));
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
